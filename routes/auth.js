874f11f9ab4b8d524371858d040017cd30b76e16
Hoomam <hoaz1600@student.miun.se>
2024-05-31 01:41:21 +0200

    if !authHeader

--- routes/auth.js
+++ routes/auth.js
@@ -43,6 +43,10 @@ router.post("/register", authTokenAndRole ,async (req, res)=>{
 function authTokenAndRole(req, res, next){
     
     const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Authorization header is missing' });
+    }
+    
     const token = authHeader.split(' ')[1];
     if (!token) return res.status(401).json({ error: 'Access denied' });
     try {
